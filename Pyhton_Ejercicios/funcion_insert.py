2920587478186014385377458b653c9d4ff3db5d
EvelynGraterol <evelynjgr@hotmail.com>
2019-01-03 23:50:04 -0300

    funcion_insert
    Insertando en dos campos

---
+++ Pyhton_Ejercicios/funcion_insert.py
@@ -0,0 +1,10 @@
+numbers_list = [1,2,3,4,5]
+
+def insert_numbers(numbers_list):
+    numbers_list.insert(5,8)
+    numbers_list.insert(6,13)
+    print('la lista es:',numbers_list)
+    return
+
+insert_numbers(numbers_list)
+print('La proxima lista:',numbers_list)
