e54556cfecab6fe7f3bd4b8ecac7b7b19a73561d
EvelynGraterol <evelynjgr@hotmail.com>
2019-01-03 23:38:00 -0300

    funcion_parametro_lista
    Se aplica como parametro una lista

--- Pyhton_Ejercicios/arreglo-indice.py
+++ Pyhton_Ejercicios/arreglo-indice.py
@@ -1,4 +1,3 @@
 arr = ([23,12,33,45,67,78])
-#arr[3:] #El numero 3 es referido a los indices, recordar que
+arr[3:] #El numero 3 es referido a los indices, recordar que
 #la indexacion en Python inicia en 0.
-arr [0:2]=34
