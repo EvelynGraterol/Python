c42da5764fecfe66945f6d7862e112b80df7c3c2
EvelynGraterol <evelynjgr@hotmail.com>
2019-03-05 00:57:26 -0300

    Create control_1.py
    Practica con elementos de control

---
+++ Pyhton_Ejercicios/control_1.py
@@ -0,0 +1,12 @@
+x = int(input("Ingrese un numero a comparar: "))
+
+if x == 3:
+    print ("x es igual a 3")
+
+elif x == 2:
+    print ("x es igual a 2")
+
+else:
+    print ("no se encuentra dentro de las opciones")
+
+print("Gracias por participar")
