71e032b1d356dc37cc291aafaa405a57ba490e73
EvelynGraterol <evelynjgr@hotmail.com>
2019-03-05 23:55:44 -0300

    For_cool
    Observar detenidamente de quien depende el número de iteraciones.

---
+++ Pyhton_Ejercicios/for_cool.py
@@ -0,0 +1,8 @@
+#Observe mediante este ejercicio, como la variable para items
+#no se toma en cuenta.Sin embargo se imprime el numero de veces
+#la palabra Cool asociada al número de caracteres que ocupa la
+#palabra 'Hello World' incluyendo el espacio, que corresponde a el número de iteraciones
+#que seran ejecutadas.
+
+for items in 'Hello World':
+    print('Cool!')
