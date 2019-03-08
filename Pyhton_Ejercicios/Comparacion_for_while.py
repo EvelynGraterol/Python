b2c083cf0713fd7862d2d2ac1f6ded23be5618d2
EvelynGraterol <evelynjgr@hotmail.com>
2019-03-07 23:54:44 -0300

    For_While
    Activa el que decidas ejecutar

---
+++ Pyhton_Ejercicios/Comparacion_for_while.py
@@ -0,0 +1,11 @@
+#Realizar un programa que imprima en pantalla los números del 0 al 100
+#Activa el método que decidas ejecutar
+
+for x in range(101):
+    print(x)
+
+
+'''x=0
+while x<101:
+    print(x)
+    x=x+1'''    
