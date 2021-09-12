5225c431e29be91986f2f3d2c4e283340b2233f5
Evelyn <evelynjgr@hotmail.com>
2021-09-11 23:39:36 -0300

    condicionales notas

--- Python_II/arc_11.py
+++ Python_II/arc_11.py
@@ -1,5 +1,23 @@
 
 """ Parte I de (11) funciones """
 # el else trabaja inmediatamente con el else que tiene mas arriba
-#cuando tenemos elif el else,  acompaña de toda la estructura
+#cuando tenemos elif, el else acompaña de toda la estructura
 
+print("Verificación de acceso")
+
+nota_alumno = int(input("Introduce tu nota, por favor: "))
+
+if nota_alumno < 5:
+    print("Insuficiente")
+
+elif nota_alumno < 6:
+    print("Suficiente")
+
+elif nota_alumno<7:
+    print("Bien")
+
+elif nota_alumno<9:
+    print("Notable")
+
+else:
+    print("Sobresaliente")
