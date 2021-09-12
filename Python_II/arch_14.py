eaaea0b8817893b3b84b0a628edc14111319676e
Evelyn <evelynjgr@hotmail.com>
2021-09-12 17:03:47 -0300

    buclesI

---
+++ Python_II/arch_14.py
@@ -0,0 +1,7 @@
+for i in [1,2,3]: #itera em función de la longitud del objeto
+    print("Hola")
+
+"""Parte II"""
+
+for estaciones_año in ["primavera", "verano","otoño","invierno"]:
+    print(estaciones_año) #aquí se manda a imprimir la variable en si misma
