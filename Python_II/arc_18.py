87b69e580ef4e4e617caae01cc6e6813b576b8ef
Evelyn <evelynjgr@hotmail.com>
2022-02-04 07:38:14 -0300

    input int

--- Python_II/arc_18.py
+++ Python_II/arc_18.py
@@ -7,3 +7,14 @@ print(f"Me alegro de conocerte, {nombre}")
 
 nombre = input("¿Cómo se llama? ")
 print(f"Me alegro de conocerle, {nombre}")
+
+
+#input de datos de tipo integer
+cantidad = int(input("Indica un valor flotante para ser llevado a int: "))
+print(f"El valor ingresa es:, {cantidad} ")
+
+
+
+
+
+#input de datos de tipo float
