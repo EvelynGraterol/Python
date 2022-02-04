8b6592fa80618061b8c00b04d2d1e273bcc8d174
Evelyn <evelynjgr@hotmail.com>
2022-02-03 22:33:33 -0300

    carga de input

---
+++ Python_II/arc_17.py
@@ -0,0 +1,19 @@
+i = 1 #inicializador
+
+while i<=10:
+
+    print("Ejecución " + str(i))
+    i = i+1 #contador o la condición de parada
+
+print("terminó la ejecución")
+
+""Parte II"""
+
+El pass devuelve un null, como si la instrucción no se ejecutase
+#ctrl+c 
+#para crear clases que ha futuro completaraemos
+#o una función 
+#es para dejar el trabajo a medias para completarlo mas tarde
+
+
+
---
+++ Python_II/arc_18.py
@@ -0,0 +1,9 @@
+#Forma 1 para ingresar los datos
+print("¿Cómo se llama usted")
+nombre = input()
+print(f"Me alegro de conocerte, {nombre}")
+
+#Forma 2 para ingresar los datos
+
+nombre = input("¿Cómo se llama? ")
+print(f"Me alegro de conocerle, {nombre}")
